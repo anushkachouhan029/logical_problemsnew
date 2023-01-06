90b6f17d8e2483f0de87e806dd11e03d28c9b92f
anushkachouhan029 <anushkachouhan029>
2023-01-06 21:33:04 +0530

    added fibonacci series

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Logicalprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number");
+            double num = Convert.ToDouble(Console.ReadLine());
+            int sum = 0;
+            Console.WriteLine("Series are as follows");
+            for (int i = 0; i <= num; i++)
+            {
+                int[] number = { i };
+                sum += i;
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
