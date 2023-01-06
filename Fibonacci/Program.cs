f32af22d699f4fab22a4583f0838fbf6d474fd4c
anushkachouhan029 <anushkachouhan029>
2023-01-06 23:34:15 +0530

    added coupan

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -1,19 +1,22 @@
-﻿namespace Logicalprogram
+﻿using System.Diagnostics;
+
+namespace Logicalprogram
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a Number to reverse");
-            int Num = Convert.ToInt32(Console.ReadLine());
-            int Reverse = 0;
-            while (Num > 0)
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[6];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
             {
-                int remainder = Num % 10;
-                Reverse = (Reverse * 10) + remainder;
-                Num = Num / 10;
+                stringChars[i] = chars[random.Next(chars.Length)];
             }
-            Console.WriteLine("Reverse No. is {0}", Reverse);
+
+            var finalString = new String(stringChars);
+            Console.WriteLine(finalString);
         }
     }
 }
